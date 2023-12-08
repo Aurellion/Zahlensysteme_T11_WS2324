cd6eb29a25aa75d86d78af1936559fb48d432994
Aurellion <johannes.bock@nastor.eu>
2023-12-08 08:17:05 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Zahlensysteme_T11_WS2324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nochmal = "";
+            string auswahl = "";
+            do
+            {
+                Console.WriteLine("1: Dezimalzahl zu Binärzahl");
+                Console.WriteLine("2: Binärzahl zu Dezimalzahl");
+
+                Console.WriteLine("Auswahl:");
+                auswahl=Console.ReadLine();
+                switch (auswahl)
+                {
+                    case "1":
+                        //Eingabe
+                        Console.WriteLine("Dezimalzahl eingeben:");
+                        int dezimalzahl = Convert.ToInt32(Console.ReadLine());
+                        //Verarbeitung
+
+                        //Ausgabe
+
+                    break;
+                }
+
+
+                Console.WriteLine("Neue Auswahl? (j/n)");
+                nochmal= Console.ReadLine();
+            } while (nochmal == "j" || nochmal == "J");
+            Console.WriteLine("Zahlensysteme:");
+        }
+    }
+}
