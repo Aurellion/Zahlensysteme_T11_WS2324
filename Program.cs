2961ba037cb5fa07f32cfa80a24c95a7f939f2b1
Aurellion <johannes.bock@nastor.eu>
2023-12-08 09:29:11 +0100

    08.12.23

--- Program.cs
+++ Program.cs
@@ -11,26 +11,63 @@
                 Console.WriteLine("1: Dezimalzahl zu Binärzahl");
                 Console.WriteLine("2: Binärzahl zu Dezimalzahl");
 
-                Console.WriteLine("Auswahl:");
+                string binärzahl = "";
+                int dezimalzahl;
+
+                Console.Write("Auswahl:");
                 auswahl=Console.ReadLine();
+
                 switch (auswahl)
                 {
                     case "1":
                         //Eingabe
                         Console.WriteLine("Dezimalzahl eingeben:");
-                        int dezimalzahl = Convert.ToInt32(Console.ReadLine());
+                        dezimalzahl = Convert.ToInt32(Console.ReadLine());
                         //Verarbeitung
-
+                        binärzahl = DezimalZuBinär(dezimalzahl);
                         //Ausgabe
-
+                        Console.WriteLine($"[{dezimalzahl}]_10 = [{binärzahl}]_2");
                     break;
+                        case "2":
+                        //Eingabe
+                        Console.WriteLine("Binärzahl eingeben:");
+                        binärzahl = Console.ReadLine();
+                        //Verarbeitung
+                        dezimalzahl = BinärZuDezimal(binärzahl);
+                        //Ausgabe
+                        Console.WriteLine($"[{binärzahl}]_2 = [{dezimalzahl}]_10");
+                        break;
                 }
-
-
                 Console.WriteLine("Neue Auswahl? (j/n)");
                 nochmal= Console.ReadLine();
             } while (nochmal == "j" || nochmal == "J");
             Console.WriteLine("Zahlensysteme:");
+        }
+
+        static int BinärZuDezimal(string binärzahl)
+        {
+            int dezimalzahl = 0;
+            for (int i = binärzahl.Length-1; i>=0; i--) //1000
+            {
+                int bin = Convert.ToInt32(binärzahl[?].ToString());                
+                dezimalzahl += bin * (int)Math.Pow(2,i);
+                Console.WriteLine(i + " " + bin + " " + dezimalzahl);
+            }
+            return dezimalzahl;
+        }
+
+        static string DezimalZuBinär(int dezimalzahl)
+        {
+            string binärzahl="";
+            int rest;
+            int dividend = dezimalzahl;
+            while (dividend != 0)
+            {
+                rest = dividend % 2;//Rest der Division
+                dividend /= 2;//Neuer Dividend
+                binärzahl = rest + binärzahl;//String von links aufbauen 
+            }
+            return binärzahl;
         }
     }
 }
