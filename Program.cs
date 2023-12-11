4e31742801f246e69aad9a5b6aabcd8e1b674f50
Aurellion <johannes.bock@nastor.eu>
2023-12-11 08:21:35 +0100

    11.12.23_a

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@
             {
                 Console.WriteLine("1: Dezimalzahl zu Binärzahl");
                 Console.WriteLine("2: Binärzahl zu Dezimalzahl");
+                Console.WriteLine("3: Dezimalzahl zu Hexadezimalzahl");
+                Console.WriteLine("4: Hexadezimalzahl zu Dezimalzahl");
+
 
                 string binärzahl = "";
+                string hexadezimalzahl = "";
                 int dezimalzahl;
 
                 Console.Write("Auswahl:");
@@ -37,6 +41,24 @@
                         //Ausgabe
                         Console.WriteLine($"[{binärzahl}]_2 = [{dezimalzahl}]_10");
                         break;
+                    case "3":
+                        //Eingabe
+                        Console.WriteLine("Dezimalzahl eingeben:");
+                        dezimalzahl = Convert.ToInt32(Console.ReadLine());
+                        //Verarbeitung
+                        hexadezimalzahl = DezimalZuHexadezimal(dezimalzahl);
+                        //Ausgabe
+                        Console.WriteLine($"[{dezimalzahl}]_10 = [{hexadezimalzahl}]_16");
+                        break;
+                    case "4":
+                        //Eingabe
+                        Console.WriteLine("Hexadezimalzahl eingeben:");
+                        binärzahl = Console.ReadLine();
+                        //Verarbeitung
+                        dezimalzahl = HexadezimalZuDezimal(hexadezimalzahl);
+                        //Ausgabe
+                        Console.WriteLine($"[{hexadezimalzahl}]_16 = [{dezimalzahl}]_10");
+                        break;
                 }
                 Console.WriteLine("Neue Auswahl? (j/n)");
                 nochmal= Console.ReadLine();
@@ -44,14 +66,28 @@
             Console.WriteLine("Zahlensysteme:");
         }
 
+        private static int HexadezimalZuDezimal(string hexadezimalzahl)
+        {
+            int dezimalzahl = 0;
+            //...
+            return dezimalzahl;
+        }
+
+        private static string DezimalZuHexadezimal(int dezimalzahl)
+        {
+            string hexadezimalzahl = "";
+            //...
+            return hexadezimalzahl;
+        }
+
         static int BinärZuDezimal(string binärzahl)
         {
             int dezimalzahl = 0;
             for (int i = binärzahl.Length-1; i>=0; i--) //1000
             {
-                int bin = Convert.ToInt32(binärzahl[?].ToString());                
+                int bin = Convert.ToInt32(binärzahl[binärzahl.Length-1-i].ToString());                
                 dezimalzahl += bin * (int)Math.Pow(2,i);
-                Console.WriteLine(i + " " + bin + " " + dezimalzahl);
+                //Console.WriteLine(i + " " + bin + " " + dezimalzahl);
             }
             return dezimalzahl;
         }
@@ -68,6 +104,20 @@
                 binärzahl = rest + binärzahl;//String von links aufbauen 
             }
             return binärzahl;
+        }
+
+        static int BuchstabeZuZahl(string Buchstabe)
+        {
+            int zahl=0;
+            //...
+            return zahl;
+        }
+
+        static string ZahlZuBuchstabe(int zahl)
+        {
+            string Buchstabe="";
+            //...
+            return Buchstabe;
         }
     }
 }
